258039cad06976a3796d96c67b992e0b6ff318c1
Felix MIL <felix.mil@esqlabs.com>
2023-05-26 11:29:06 +0200

    run dev/01_start.R

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "TissueTMDD", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
+  pkg_title = "A shiny Application to Interact with drug-target binding in tissue interstitial space model", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL, # The URL of the GitHub Repo (optional),
+  author_first_name = "Felix", # Your First Name
+  author_last_name = "MIL", # Your Last Name
+  author_email = "felix.mil@esqlabs.com", # Your Email
+  repo_url = "https://github.com/esqLABS/TissueTMDD", # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
 
@@ -37,12 +37,12 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("Felix MIL") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Felix MIL")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
@@ -55,8 +55,8 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
-# golem::remove_favicon() # Uncomment to remove the default favicon
+# golem::use_favicon() # path = "path/to/ico". Can be an online file.
+golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
 golem::use_utils_ui(with_test = TRUE)
