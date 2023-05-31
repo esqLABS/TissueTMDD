0af9e91ef631801ce2146defc61efc247127d2ee
Felix MIL <felix.mil@esqlabs.com>
2023-05-31 12:03:40 +0200

    Add module that handles launching simulations

--- R/mod_run_launcher.R
+++ R/mod_run_launcher.R
@@ -4,28 +4,34 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_run_launcher_ui <- function(id){
   ns <- NS(id)
-  tagList(
- 
-  )
+    bs4Dash::actionButton(inputId = ns("run_simulation_btn"),
+                          label = "Run",
+                          icon = icon("play"),
+                          width = "100%")
 }
-    
+
 #' run_launcher Server Functions
 #'
-#' @noRd 
-mod_run_launcher_server <- function(id){
+#' @noRd
+mod_run_launcher_server <- function(id, r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
+    observeEvent(input$run_simulation_btn,{
+      message("Run simulation button pressed")
+      r$run_simulation <- input$run_simulation_btn
+    })
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_run_launcher_ui("run_launcher_1")
-    
+
 ## To be copied in the server
 # mod_run_launcher_server("run_launcher_1")
