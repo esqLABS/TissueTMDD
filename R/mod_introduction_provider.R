0dcef57acb7ec0dd4274a19de0240cd9ec9b37bd
Felix MIL <felix.mil@esqlabs.com>
2023-06-13 15:53:42 +0200

    Provide link to pkml file + Add latex for variables

--- R/mod_introduction_provider.R
+++ R/mod_introduction_provider.R
@@ -14,17 +14,25 @@ mod_introduction_provider_ui <- function(id) {
     icon = icon("info"),
     width = 12,
     collapsible = TRUE,
-    tags$small(
-      "This model is based on the PK-Sim model for large molecules in humans and
+    withMathJax(),
+    helpText(
+      tags$b(
+        HTML(
+          "This <a href='https://github.com/esqLABS/TissueTMDD/blob/main/inst/extdata/Large%20molecule%20Human%20default.pkml'>model</a>
+      is based on the PK-Sim model for large molecules in humans and
       is extended in Mobi with target binding to a target expressed in the
-      interstitial space of an organ. The target is degraded with a first-order
-      rate constant kdeg and synthesized with a zero-order rate constant ksyn,
+      interstitial space of an organ.")),
+      HTML( "<br>
+      The target is degraded with a first-order rate constant \\(k_{deg}\\)
+      and synthesized with a zero-order rate constant \\(k_{syn}\\)
       which is calculated as the product of the target concentration in the
-      interstitial space and the kdeg. The drug-target complex is degraded with
-      a first-order rate constant kint. Various outputs can be selected in the
-      plot options.",
-      br(),
-      "Disclaimer: no rights can be derived from this model or any of its output"
+      interstitial space and the \\(k_{deg}\\).
+      <br>
+      The drug-target complex is degraded with a first-order rate constant \\(k_{int}\\).
+      <br>
+      Various outputs can be selected in the plot options.
+      <br>
+      Disclaimer: no rights can be derived from this model or any of its output.")
     )
   )
 }
