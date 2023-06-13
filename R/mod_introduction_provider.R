ac316026342f7f0c55ebbf29a4b17af3447909f8
Felix MIL <felix.mil@esqlabs.com>
2023-06-13 15:09:20 +0200

    Add introductory text (#10)

--- R/mod_introduction_provider.R
+++ R/mod_introduction_provider.R
@@ -15,7 +15,16 @@ mod_introduction_provider_ui <- function(id) {
     width = 12,
     collapsible = TRUE,
     tags$small(
-      shinipsum::random_text(nwords = 50)
+      "This model is based on the PK-Sim model for large molecules in humans and
+      is extended in Mobi with target binding to a target expressed in the
+      interstitial space of an organ. The target is degraded with a first-order
+      rate constant kdeg and synthesized with a zero-order rate constant ksyn,
+      which is calculated as the product of the target concentration in the
+      interstitial space and the kdeg. The drug-target complex is degraded with
+      a first-order rate constant kint. Various outputs can be selected in the
+      plot options.",
+      br(),
+      "Disclaimer: no rights can be derived from this model or any of its output"
     )
   )
 }
