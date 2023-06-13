e31be8f87e08d597bab5a174d2ec7c12c2985bae
Felix MIL <felix.mil@esqlabs.com>
2023-06-13 10:30:44 +0200

    Updated default preset data

--- data-raw/default.R
+++ data-raw/default.R
@@ -4,10 +4,18 @@ library(ospsuite)
 
 default_model <- load_default_model()
 
+default_settings <- init_parameters()
+
+for (p in default_settings) {
+  update_model_parameters(default_model,
+                          parameter_path = p$path,
+                          value = p$value)
+}
+
 simulation_results <- ospsuite::runSimulation(default_model)
 
 default_data <- ospsuite::getOutputValues(simulation_results,
-                          quantitiesOrPaths = output_paths()
+                                          quantitiesOrPaths = output_paths()
 )$data
 
 usethis::use_data(default_data, overwrite = TRUE)
