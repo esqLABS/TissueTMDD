793b28242cfe38bf946289c477de96a1172a349f
Felix MIL <felix.mil@esqlabs.com>
2023-07-24 10:18:17 +0200

    Update default data generation script with latest changes

--- data-raw/default.R
+++ data-raw/default.R
@@ -4,14 +4,14 @@ devtools::load_all(".")
 default_model <- load_default_model()
 
 default_parameters_paths <- unname(get_parameters_paths())
-default_values <- unname(get_parameters_default_value())
+default_values <- get_parameters_path_default_values()
 
 for (organ in get_organs()) {
   default_parameters_paths <- c(default_parameters_paths,
                        glue::glue("Organism|{organ}|Intracellular|Target|Relative expression")
   )
 
-  if (organ != default_parameters()$organ) {
+  if (organ != get_parameters_default_value()["organ"]) {
     default_values <- c(default_values, 0)
   } else {
     default_values <- c(default_values, 1)
@@ -22,7 +22,7 @@ output_paths <- vector(mode = "character", length = length(defaut_output_paths()
 
 for (i in seq_along(defaut_output_paths())) {
   if (stringr::str_detect(defaut_output_paths()[i], "Organism",negate = TRUE)) {
-    output_paths[i] <- glue::glue("Organism|{default_parameters()$organ}|{defaut_output_paths()[i]}")
+    output_paths[i] <- glue::glue("Organism|{ get_parameters_default_value()['organ']}|{defaut_output_paths()[i]}")
   } else {
     output_paths[i] <- defaut_output_paths()[i]
   }
